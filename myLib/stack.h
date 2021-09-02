32f46166a7a4fd88ebcfaaa0da42660009ac7475
Dumpinground <e605336584@icloud.com>
2021-09-02 22:32:11 +0800

    Stack Update

--- myLib/stack.h
+++ myLib/stack.h
@@ -7,19 +7,25 @@
 
 #define MaxSize 10
 
-typedef int Elemtype;
+typedef int ElemType;
 
 //顺序栈
 struct SqStack {
-    Elemtype data[MaxSize];
+    ElemType data[MaxSize];
     int top;
 };
 
 //共享栈
 struct ShStack {
-    Elemtype x;
+    ElemType x;
     int top0, top1;
 };
+
+//链栈
+typedef struct LinkNode {
+    ElemType data;
+    struct LinkNode *next;
+} *LiStack;
 
 void InitStack(SqStack &S) {
     S.top = -1;
@@ -30,9 +36,22 @@ void InitStack(ShStack &S) {
     S.top1 = MaxSize; //从顶部开始
 }
 
+void InitStack(LiStack &S) {
+    S = (LinkNode *) malloc(sizeof(LinkNode));
+    S->next = NULL;
+}
+
+bool StackEmpty(SqStack S) {
+    return S.top == -1;
+}
+
+bool StackEmpty(LiStack S) {
+    return S->next == NULL;
+}
+
 void DestroyStack(SqStack &S);
 
-bool Push(SqStack &S, Elemtype x) {
+bool Push(SqStack &S, ElemType x) {
 
     if (S.top == MaxSize - 1)
         return false;
@@ -43,7 +62,17 @@ bool Push(SqStack &S, Elemtype x) {
     return true;
 }
 
-bool Pop(SqStack &S, Elemtype &x) {
+bool Push(LiStack &S, ElemType x) {
+    LinkNode *n = (LinkNode *) malloc(sizeof(LinkNode));
+    if (!n)
+        return false;
+    n->data = x;
+    n->next = S->next;
+    S->next = n;
+    return true;
+}
+
+bool Pop(SqStack &S, ElemType &x) {
 
     if (S.top == -1)
         return false;
@@ -51,15 +80,52 @@ bool Pop(SqStack &S, Elemtype &x) {
     return true;
 }
 
-bool GetTop(SqStack S, Elemtype &x) {
+bool Pop(LiStack &S, ElemType &x) {
+
+    if (StackEmpty(S))
+        return false;
+
+    LinkNode *n = S->next;
+    x = n->data;
+    S->next = n->next;
+    free(n);
+    return true;
+}
+
+bool GetTop(SqStack S, ElemType &x) {
     if (S.top == -1)
         return false;
     x = S.data[S.top];
     return true;
 }
 
-bool StackEmpty(SqStack S) {
-    return S.top == -1;
+bool GetTop(LiStack S, ElemType &x) {
+
+    if (StackEmpty(S))
+        return false;
+
+    x = S->next->data;
+    return true;
+}
+
+std::ostream &operator<<(std::ostream &out, SqStack S) {
+    ElemType x;
+    out << "SqStack:" << std::endl;
+    while (S.top != -1) {
+        Pop(S, x);
+        out << x << std::endl;
+    }
+    return out;
+}
+
+std::ostream &operator<<(std::ostream &out, LiStack S) {
+    LinkNode *n = S;
+    out << "LiStack:" << std::endl;
+    while (n->next) {
+        n = n->next;
+        out << n->data << std::endl;
+    }
+    return out;
 }
 
 #endif //ALGORITHMEXERCISE_STACK_H
--- test/3.1.4-Stack.cpp
+++ test/3.1.4-Stack.cpp
@@ -5,7 +5,54 @@
 #include <gtest/gtest.h>
 #include "../myLib/stack.h"
 
-TEST(Stack, 0) {
+using namespace std;
+
+TEST(Stack, testSqStack) {
     SqStack S;
+    int e;
     InitStack(S);
+    cout << "Empty: " << StackEmpty(S) << endl;
+    for (int i = 0; i < 10; ++i) {
+        Push(S, i);
+    }
+    cout << S << endl;
+    Pop(S, e);
+    cout << S << endl;
+    cout << "Top: " << S.top << " Empty: " << StackEmpty(S) << endl;
+}
+
+TEST(Stack, testLiStack) {
+    LiStack S;
+    int e;
+    InitStack(S);
+    cout << "Empty: " << StackEmpty(S) << endl;
+    for (int i = 0; i < 10; ++i) {
+        Push(S, i);
+    }
+    cout << S << endl;
+    Pop(S, e);
+    cout << S << endl;
+    GetTop(S, e);
+    cout << "Top: " << e << " Empty: " << StackEmpty(S) << endl;
+}
+
+bool isLegal(char a[]) {
+    int stackSize = 0;
+    for (int i = 0; i < sizeof(a) / sizeof('a'); ++i) {
+        switch (a[i]) {
+            case 'I':
+                stackSize++;
+                break;
+            case 'O':
+                stackSize--;
+        }
+        if (stackSize < 0)
+            return false;
+    }
+    return stackSize == 0;
+}
+
+TEST(Stack, 3) {
+    char a[] = "IOIIOIOO", b[] = "IOOIOIIO", c[] = "IIIOIOIO", d[] = "IIIOOIOO";
+    cout << isLegal(a) << " " << isLegal(b) << " " << isLegal(c) << " " << isLegal(d) << endl;
 }
